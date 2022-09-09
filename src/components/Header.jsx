f6995b11d479df9c93e9e8bb36acbf54194a51f8
Daniel <daniel.gardiner@six.agency>
2022-09-09 14:21:18 +0100

    more styling stuffzz

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Header({ session }) {
   return (
     <div className="w-full flex justify-center bg-gray-100 border-b-2 border-gray-300 mb-11">
-      <div className="w-full max-w-4xl flex justify-between p-4">
+      <div className="w-full max-w-7xl flex justify-between p-4">
         <div className="flex items-center">
           <Link href='/' >
             <Image src={TravelIcon} alt="Vercel Logo" width={40} height={40} className="cursor-pointer" />
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ export default function Layout({ children, session }) {
     <>
       <Header session={session} />
       <div className="w-full flex justify-center min-h-screen">
-        <div className="w-full max-w-4xl px-4">
+        <div className="w-full max-w-7xl px-4">
           {children}
         </div>
       </div>
