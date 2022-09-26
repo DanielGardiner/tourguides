ccb185ee1b02b0b46dedbc024c70b089cc9b9c83
Daniel <daniel.gardiner@six.agency>
2022-09-26 22:45:55 +0100

    make use of suspense boundary

---
+++ src/components/ToursGrid.jsx
@@ -0,0 +1,14 @@
+import useGetTours from "../hooks/useGetTours";
+import Card from "./Card";
+
+export default function ToursGrid() {
+  const { data: tours } = useGetTours({suspense: true});
+
+  return (
+    <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+      {tours?.map((tour) => (
+        <Card tour={tour} key={tour.id} />
+      ))}
+    </div>
+  );
+}
--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,27 +1,14 @@
-import { getSession, useSession } from "next-auth/react";
+import { Suspense } from "react";
 import Layout from "../components/Layout";
-import tourService from "../server/services/tour";
-import useGetTours from "../hooks/useGetTours";
-import Card from "../components/Card";
-import useGetSession from "../hooks/useGetSession";
 import LoadingSpinner from "../components/LoadingSpinner";
+import ToursGrid from "../components/ToursGrid";
 
 export default function ToursPage() {
-  // const session = useSession()
-  const { data: session, isLoading: isSessionLoading } = useGetSession();
-  const { data: tours, isLoading: isToursLoading } = useGetTours();
-
   return (
     <Layout>
-      {isSessionLoading || isToursLoading ? (
-        <LoadingSpinner />
-      ) : (
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
-          {tours?.map((tour) => (
-            <Card tour={tour} key={tour.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<LoadingSpinner />}>
+        <ToursGrid />
+      </Suspense>
     </Layout>
   );
 }
